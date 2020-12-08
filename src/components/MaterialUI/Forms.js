39764cb2c6c288bed25eafd548eae058066ff1d3
Pedro <pedrolucinda2020@gmail.com>
2020-12-08 14:49:56 -0300

    Edit Profile Estrutura pronta

--- src/components/MaterialUI/Forms.js
+++ src/components/MaterialUI/Forms.js
@@ -5,18 +5,17 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles((theme) => ({
   textField: {
     width: "300px",
-    border: "1px solid var(--main)",
   },
 }));
 
-export default function InputForm() {
+export default function InputForm(props) {
   const classes = useStyles();
   return (
     <TextField
       className={classes.textField}
       id="outlined-search"
-      label="Search field"
-      type="search"
+      label={props.label}
+      type={props.type}
       variant="outlined"
     />
   );
--- src/pages/ProfilePages/EditProfile/EditProfile.js
+++ src/pages/ProfilePages/EditProfile/EditProfile.js
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import InputForm from "../../../components/MaterialUI/Forms";
+import { Container } from "../../../components/StyledComponents/styles";
+import TopBar from "../../../components/TopBar/TopBar";
+import { Button } from "../../../components/MaterialUI/Buttons";
+import { Form } from "./style";
 const EditProfile = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+  return (
+    <Container>
+      <TopBar Title="Editar Perfil" />
+      <Form>
+        <InputForm label="Nome" type={"text"} />
+        <br />
+        <InputForm label="Email" type={"email"} />
+        <br />
+        <InputForm label="CPF" type={"number"} />
+        <br />
+        <Button> Salvar </Button>
+      </Form>
+    </Container>
+  );
+};
 
-export default EditProfile
+export default EditProfile;
