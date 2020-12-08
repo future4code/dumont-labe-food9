cd8811db2db9c41ed0c1b3586621d1ecd55f9331
Pedro <pedrolucinda2020@gmail.com>
2020-12-08 12:25:34 -0300

    Material UI complete

--- src/components/MaterialUI/Loading.js
+++ src/components/MaterialUI/Loading.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
-export const Loading = () => {
-	return (
-		<div>
-			
-		</div>
-	)
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > * + *": {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+export default function Loading() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CircularProgress style={{ color: "green" }} />
+    </div>
+  );
 }
