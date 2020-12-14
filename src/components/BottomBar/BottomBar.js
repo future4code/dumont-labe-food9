e484de36ac0fb5b298c84af4c0598f9776a4aaec
Pedro <pedrolucinda2020@gmail.com>
2020-12-14 11:45:59 -0300

    Bottom bar working

--- src/components/BottomBar/BottomBar.js
+++ src/components/BottomBar/BottomBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BsHouseDoor, BsBag, BsPerson } from "react-icons/bs";
+import { useHistory } from "react-router-dom";
 import "./bottombar.css";
 
 const BottomBar = () => {
+  const history = useHistory();
+
   return (
     <div className="bottombar_container">
-      <BsHouseDoor />
-      <BsBag />
-      <BsPerson />
+      <BsHouseDoor  onClick={() => history.push("/home")}/>
+      <BsBag onClick={() => history.push("/cart")} />
+      <BsPerson onClick={() => history.push("/profile")} />
     </div>
   );
 };
